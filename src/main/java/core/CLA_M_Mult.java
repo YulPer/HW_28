535cc3f9447976d065e4cb3f2f5e0fca0b269c1a
Yul Per <yuliiaperedrii.qa@gmail.com>
2019-06-13 15:15:26 -0700

    First commit

---
+++ src/main/java/core/CLA_M_Mult.java
@@ -0,0 +1,11 @@
+package core;
+import java.math.*;
+
+public class CLA_M_Mult {
+	public static void main (String [] cla){
+	if (cla.length < 2) {System.out.println("At least two parameters"); System.exit(0);	}
+	Double result = Double.valueOf(cla [0]);
+	for (int i = 1; i <cla.length; i++) {result *= Double.valueOf(cla [i]);}
+	System.out.println(new BigDecimal(result).setScale(2, RoundingMode.HALF_UP));
+	}
+}
---
+++ src/main/java/core/CLA_S.java
@@ -0,0 +1,7 @@
+package core;
+
+public class CLA_S {
+	public static void main (String [] cla) {
+		System.out.println("Hello " + cla [0]);
+	}
+}
